a1486fbdc3fc004ccfb0f55715d8c310759eb700
loking23 <mongrock30102@gmail.com>
2023-09-09 23:50:00 +0800

    feature: 調整 addComma 邏輯
    加上小數點保留邏輯
    原本 toLocaleString 會自動 formatted 掉小數點，當沒有任何小數的時候

--- src/utils/addComma.js
+++ src/utils/addComma.js
@@ -1,5 +1,15 @@
 // input: -7855948.9527
 // output: -7,855,948.9527
-const addComma = (num, maximumFractionDigits = 20) => num.toLocaleString('en-US', { maximumFractionDigits });
+const addComma = (num, maximumFractionDigits = 20) => {
+    const isInvalid = isNaN(num);
+
+    if(isInvalid) return num;
+
+    const [integer, decimal] = String(num).split('.');
+    const integerWithComma = (+integer).toLocaleString('en-US', { maximumFractionDigits });
+    const result = decimal !== undefined ? `${integerWithComma}.${decimal}` : integerWithComma;
+
+    return result;
+}
 
 export default addComma
