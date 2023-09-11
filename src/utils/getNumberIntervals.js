d91e41386bc575bbc03dfeae9d90448973c5ae01
loking23 <mongrock30102@gmail.com>
2023-09-11 09:58:55 +0800

    fix: getNumberIntervals 取得正確區間

--- src/utils/getNumberIntervals.js
+++ src/utils/getNumberIntervals.js
@@ -2,56 +2,59 @@
 // output: {overlap: [[6, 8], [17, 17]], notInclude: [[0, 4], [12, 13]]}
 
 const getOverlap = (array) => {
-    const ascendingArray = structuredClone(array).sort((a, b) => a[0] - b[0]);
-    // 下個元素的 min < 當前的 max，表示 overlap
-    const overlapRangeArray = ascendingArray.reduce((acc, cur, index) => {
-        const [_, curMax] = cur;
-        const [nextMin, nextMax] = ascendingArray[index + 1] || [Infinity];
-        const isOverlap = nextMin <= curMax;
-        const overlapMax = nextMax < curMax ? nextMax : curMax;
+  const ascendingArray = structuredClone(array).sort((a, b) => a[0] - b[0]);
+  // 下個元素的 min < 當前的 max，表示 overlap
+  const overlapRangeArray = ascendingArray.reduce((acc, cur, index) => {
+    const [_, curMax] = cur;
+    const [nextMin, nextMax] = ascendingArray[index + 1] || [Infinity];
+    const isOverlap = nextMin <= curMax;
+    const overlapMax = nextMax < curMax ? nextMax : curMax;
 
-        if(isOverlap) acc.push([nextMin, overlapMax]);
+    if (isOverlap) acc.push([nextMin, overlapMax]);
 
-        return acc;
-    }, []);
+    return acc;
+  }, []);
 
-    return overlapRangeArray
-}   
+  return overlapRangeArray;
+};
 
 const getNotInclude = (array, min = 1, max = 20) => {
-    const result = [];
-    const ascendingArray = structuredClone(array).sort((a, b) => a[0] - b[0]);
-    let prevMax = min;
+    // 找出未包含的區間
+    const notInclude = [];
+    let currentInterval = null;
     
-    for(let i = 0;i<ascendingArray.length;i++) {
-        const isFinished = prevMax >= max;
-        const [nextMin] = ascendingArray[i + 1] || [Infinity];
-
-        // 跑完 min~max 範圍，後面就不用再跑了
-        if(isFinished) return result
-
-        // 與下個元素之間有空的 range
-        if(nextMin > prevMax) {
-            const rangeMin = prevMax + 1;
-            const rangeMax = nextMin < max ? nextMin - 1 : max;
-            const notIncludeRange = [rangeMin, rangeMax];
-
-            result.push(notIncludeRange);
+    for (let i = min; i <= max; i++) {
+      const isCovered = array.some(([start, end]) => i >= start && i <= end);
+      
+      if (!isCovered) {
+        if (currentInterval === null) {
+          currentInterval = [i, i];
+        } else {
+          if (i === currentInterval[1] + 1) {
+            currentInterval[1] = i;
+          } else {
+            notInclude.push(currentInterval);
+            currentInterval = [i, i];
+          }
         }
-
-        prevMax = nextMin
+      }
     }
+  
+    // 如果最後還有一個未包含的區間，加入結果中
+    if (currentInterval !== null) {
+      notInclude.push(currentInterval);
+    }
+  
+    return notInclude;
+  };
+  
 
-    return result
-}
+const getNumberIntervals = (array, min = 1, max = 20) => {
+  const overlap = getOverlap(array);
+  const notInclude = getNotInclude(array, min, max);
+  const result = { overlap, notInclude };
 
-const getNumberIntervals = (array, min = 0, max = 20) => {
-    const overlap = getOverlap(array);
-    const notInclude = getNotInclude(array, min, max);
-    const result = { overlap, notInclude };
+  return result;
+};
 
-    return result
-}
-
-export default getNumberIntervals
-
+export default getNumberIntervals;
